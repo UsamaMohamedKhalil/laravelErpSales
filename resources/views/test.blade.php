2755cfe91aba936f7532f2c7067a7bd49a619b5d
UsamaMohamedKhalil <usamamohamed1222@gmail.com>
2024-12-14 23:58:29 +0200

    1st Commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('title')
+test view title
+@endsection
+
+@section('contentheader')
+test head 1
+@endsection
+
+@section('contentheaderlink')
+<a herf="#"> link</a>
+@endsection
+
+@section('contentheaderactive')
+this is test
+@endsection
+
+@section('content')
+test view
+@endsection
